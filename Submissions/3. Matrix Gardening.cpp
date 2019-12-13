1e70fbbbf810a25937507f2fcd93fc397b38a6a2
Aadhav Kandasamy Suresh <43770524+aadhavksuresh@users.noreply.github.com>
2019-12-13 18:24:04 +0530

    Update 3. Matrix Gardening.cpp

--- Submissions/3. Matrix Gardening.cpp
+++ Submissions/3. Matrix Gardening.cpp
@@ -3,38 +3,54 @@
 
 using namespace std;
 
-int calcarea(bool mato[N][N],int n)
+int main()
 {
-    int bmat1[n+1][n+1];
+    bool A[N][N];
 
-    for(int i=0;i<n;i++)
+    int B[N][N];
+
+    int carea,finarea=0;
+
+    int x,y,n;
+
+    cin>>n;
+
+    for(x=0;x<n;x++)
     {
-        bmat1[0][i]=mato[0][i];
-
-        for(int j=1;j<n;j++)
+        for(y=0;y<n;y++)
         {
-            bmat1[j][i]=(mato[j][i]==0)?0:bmat1[j-1][i]+1;
+            cin>>A[x][y];
         }
     }
 
     for(int i=0;i<n;i++)
     {
-        int con[n+1]={0};
+        B[0][i]=A[0][i];
+
+        for(int j=1;j<n;j++)
+        {
+            B[j][i]=(A[j][i]==0)?0:B[j-1][i]+1;
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        int A[n+1]={0};
 
         for(int j=0;j<n;j++)
         {
-            con[bmat1[i][j]]++;
+            A[B[i][j]]++;
         }
 
         int coln=0;
 
         for(int j=n;j>=0;j--)
         {
-            if(con[j]>0)
+            if(A[j]>0)
             {
-                for(int k=0;k<con[j];k++)
+                for(int k=0;k<A[j];k++)
                 {
-                    bmat1[i][coln]=j;
+                    B[i][coln]=j;
 
                     coln++;
                 }
@@ -42,13 +58,11 @@ int calcarea(bool mato[N][N],int n)
         }
     }
 
-    int carea,finarea=0;
-
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            carea=(j+1)*bmat1[i][j];
+            carea=(j+1)*B[i][j];
 
             if(carea>finarea)
             {
@@ -57,27 +71,7 @@ int calcarea(bool mato[N][N],int n)
         }
     }
 
-    return finarea;
-
-}
-
-int main()
-{
-    bool areamat[N][N];
-
-    int x,y,z;
-
-    cin>>z;
-
-    for(x=0;x<z;x++)
-    {
-        for(y=0;y<z;y++)
-        {
-            cin>>areamat[x][y];
-        }
-    }
-
-    cout <<calcarea(areamat,z);
+    cout<<finarea;
 
     return 0;
 }
